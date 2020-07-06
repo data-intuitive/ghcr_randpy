afbe1c4193e893a852248f3ed7c49e8e97b9b732
Robrecht Cannoodt <rcannood@gmail.com>
2020-07-06 12:38:46 +0200

    only tag major releases; reshuffle files

--- src/generate.sh
+++ src/generate.sh
@@ -4,9 +4,9 @@ fol_os=src/1_os
 fol_r=src/2_r
 fol_py=src/3_python
 for os in buster; do
-  for r in 3.6.3 4.0.2; do
-    for py in 3.6.11 3.7.8 3.8.3; do
-      out=dockerfiles/${os}_r${r}_py${py}.Dockerfile
+  for r in 3.6 4.0; do
+    for py in 3.6 3.7 3.8; do
+      out=dockerfiles/r${r}_py${py}.Dockerfile
       cat $fol_os/$os/Dockerfile > $out
       echo >> $out
       cat $fol_r/$r/Dockerfile >> $out
