1c7f5aa2edb866b66fa7d493bc8e250ba6036ed7
Robrecht Cannoodt <rcannood@gmail.com>
2020-07-06 11:34:21 +0200

    move files around; try tagging r and python as latest

--- src/generate.sh
+++ src/generate.sh
@@ -6,9 +6,11 @@ fol_py=src/3_python
 for os in buster; do
   for r in 3.6.3 4.0.2; do
     for py in 3.6.11 3.7.8 3.8.3; do
-      out=${os}_r${r}_py${py}.Dockerfile
+      out=dockerfiles/${os}_r${r}_py${py}.Dockerfile
       cat $fol_os/$os/Dockerfile > $out
+      echo >> $out
       cat $fol_r/$r/Dockerfile >> $out
+      echo >> $out
       cat $fol_py/$py/Dockerfile >> $out
     done
   done
